a5edf17067c04a5797faea8265e2abeb13b6be50
maksimVAs73 <traffikingvraj@gmail.com>
2024-11-20 22:48:28 +0900

    Add files via upload

---
+++ ConsoleApp1/ClassLibrary1/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        // Методы с типом void
+        public void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void AddAndPrint(int a, int b)
+        {
+            Console.WriteLine($"Сумма {a} + {b} = {a + b}");
+        }
+
+        // Методы с возвращаемыми значениями
+        public int AddIntegers(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double CalculateAverage(double a, double b)
+        {
+            return (a + b) / 2;
+        }
+
+        public string GetGreeting(string name)
+        {
+            return $"Привет, {name}!";
+        }
+    }
+}
+
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 library = new Class1();
+            library.PrintMessage("Hello My NIGGERS!");
+            library.AddAndPrint(89, 52);
+            Console.WriteLine($"Сумма 78 + 23 = {library.AddIntegers(78, 23)}");
+            Console.WriteLine($"Среднее 100 и 50: {library.CalculateAverage(100, 50)}");
+            Console.WriteLine(library.GetGreeting("Maksimen"));
+        }
+    }
+}
